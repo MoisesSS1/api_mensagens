9944d52be25bcac71ca825335a6826dfb5320be8
Patt0lino <investmentmoises@gmail.com>
2022-12-18 18:02:37 -0300

    confi routes

--- VIEW_API/src/index.js
+++ VIEW_API/src/index.js
@@ -15,7 +15,7 @@ import NoPages from './pages/NoPages'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route element={<App/>}>
         <Route path='/' element={<Home/>}/>
